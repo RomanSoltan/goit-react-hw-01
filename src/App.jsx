acd4f6f60d2276426785215249b34fcfa740c16c
Roman Soltan <romabandit88@gmail.com>
2025-01-13 01:44:01 +0200

    add task-3 jsx markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 import userData from './db/userData.json';
 import friends from './db/friends.json';
+import transactions from './db/transactions.json';
 
 const App = () => {
   return (
@@ -15,6 +17,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };

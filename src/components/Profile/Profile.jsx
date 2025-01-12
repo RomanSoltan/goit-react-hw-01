cf010de19cc9cecec7c0728db2d53c5887b56ce7
Roman Soltan <romabandit88@gmail.com>
2025-01-12 22:58:27 +0200

    add Profile.module.css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,27 @@
-// import css from './Profile.module.css';
+import css from './Profile.module.css';
 
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div>
+    <div className={css.profileCard}>
       <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+        <img className={css.profileImg} src={image} alt="User avatar" />
+        <p className={css.profileTitle}>{name}</p>
+        <p className={css.profileText}>@{tag}</p>
+        <p className={css.profileText}>{location}</p>
       </div>
 
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css.profileList}>
+        <li className={css.profileItem}>
+          <span className={css.profileSubtitle}>Followers</span>
+          <span className={css.profileStats}>{stats.followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css.profileItem}>
+          <span className={css.profileSubtitle}>Views</span>
+          <span className={css.profileStats}>{stats.views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css.profileItem}>
+          <span className={css.profileSubtitle}>Likes</span>
+          <span className={css.profileStats}>{stats.likes}</span>
         </li>
       </ul>
     </div>
